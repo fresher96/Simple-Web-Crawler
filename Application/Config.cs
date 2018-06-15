ade1ddde056a92854c1bdd4d2980ad6544d4a5c6
Farouk <nothinginvain@hotmail.com>
2018-06-15 16:43:31 +0200

    last commit

--- Application/Config.cs
+++ Application/Config.cs
@@ -12,7 +12,7 @@ namespace Application
         //public const string seed = "https://google.com/";
         //public const string seed = "http://en.wikipedia.org/wiki/Main_Page";
 
-        public const int maxNbrOfLinks = 8; // should be >= 1
+        public const int maxNbrOfLinks = 15; // should be >= 1
 
         public const string filesDirectory = "../../files";
         public const string filesPrefix = "eclass_";
@@ -20,7 +20,7 @@ namespace Application
         public const string proxyIp = "172.25.1.152";
         public const int proxyPort = 8080;
         public const string proxyUserName = "farouk.hjabo";
-        public const string proxyPassword = "1234.qwer";
+        public const string proxyPassword = "********";
 
         public static string[] absoluteLinkPatterns = { "//", "http://", "https://" };
         public static int MaxAllowedFileLength = 100000; // 10^5
